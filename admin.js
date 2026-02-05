e0c7091d5b2f17f3ada3a4a699292d0378170f83
Mouli-is-me <134188961+Mouli-is-me@users.noreply.github.com>
2026-02-05 21:22:27 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  doc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCx0gBDakjvl0v9K6UNXEemMAdZoamlQI8",
+  authDomain: "madurai-jilla.firebaseapp.com",
+  projectId: "madurai-jilla",
+  storageBucket: "madurai-jilla.firebasestorage.app",
+  messagingSenderId: "164673347054",
+  appId: "1:164673347054:web:d151919fb15a2941f202ff",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const ordersDiv = document.getElementById("orders");
+
+onSnapshot(collection(db, "orders"), (snapshot) => {
+  ordersDiv.innerHTML = "";
+  snapshot.forEach((docSnap) => {
+    const o = docSnap.data();
+    ordersDiv.innerHTML += `
+      <div style="border:1px solid #ccc;padding:10px;margin:10px">
+        <b>Table:</b> ${o.table}<br>
+        <b>Total:</b> ₹${o.total}<br>
+        <b>Status:</b> ${o.status}<br>
+        <button onclick="markDone('${docSnap.id}')">Mark Done</button>
+      </div>
+    `;
+  });
+});
+
+async function markDone(id) {
+  await updateDoc(doc(db, "orders", id), {
+    status: "DONE",
+  });
+}
